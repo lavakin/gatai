5f842ff5a59bf361e0cbc4b7e70d049600e6d085
lavakin <nikola@kalabova.eu>
2024-07-18 20:42:26 +0200

    release

--- setup.py
+++ setup.py
@@ -10,15 +10,9 @@ setup(
     project_urls = {"Documentation" :"https://trapga.readthedocs.io/en/latest/genindex.html"},
     author = 'Nikola Kalábová',              
     author_email = 'nikola@kalabova.eu',     
-<<<<<<< HEAD
     url = 'https://github.com/lavakin/gatai', 
     version = "{{VERSION_PLACEHOLDER}}",      
     #version = 1.1,
-=======
-    url = 'https://github.com/lavakin/tai_chi', 
-    #version = "{{VERSION_PLACEHOLDER}}",      
-    version = 1.1,
->>>>>>> parent of 0682a12 (release)
     keywords = ['Genetic algorithms', 'minimal subset', 'multi-objective', "optimization"],   
     install_requires=['numpy', 'scipy', 'pandas', 'argparse', 'scikit-learn', 'tqdm',"setga"],
     entry_points={
